545f76011b2839b08ea5949ccca4cb2464771884
Nahuel <nahubtiel@gmail.com>
2019-04-13 10:19:18 -0300

    casi terminado

--- EjerciciosSem5.cpp
+++ EjerciciosSem5.cpp
@@ -62,7 +62,7 @@ NodoLista* listaOrdenadaInsertionSort(NodoLista * l)
 	return insertSort(ln);
 }
 
-void listaOrdenadaSelectionSort(NodoLista*& l)
+void listaOrdenadaSelectionSort(NodoLista * &l)
 {
 	//int puntero = 0;
 	//while (!estaOrdenada(l, l->dato)) {
@@ -110,17 +110,15 @@ NodoLista* invertirParcial(NodoLista * l)
 
 		while (l1 != NULL) {
 			NodoLista* nodo = new NodoLista;
+			nodo->dato = l1->dato;
 
 			if (n == NULL) {
 				nodo->sig = NULL;
 			}
 			else {
-				nodo->dato = l1->dato;
-				nodo->sig = n->sig;
-
-				n->sig = nodo;
-
+				nodo->sig = n;
 			}
+			n = nodo;
 
 			l1 = l1->sig;
 		}
@@ -135,20 +133,138 @@ NodoLista* invertirParcial(NodoLista * l)
 
 NodoLista* intercalarIter(NodoLista * l1, NodoLista * l2)
 {
-	// IMPLEMENTAR SOLUCION
-	return NULL;
+	if (l1 == NULL) {
+		return listaNueva(l2);
+	}
+	else if (l2 == NULL) {
+		return listaNueva(l1);
+	}
+	else {
+		NodoLista* lis = new NodoLista;
+		NodoLista* last = lis;
+
+		NodoLista* ll1 = l1;
+		NodoLista* ll2 = l2;
+
+		if (ll1->dato <= ll2->dato) {
+			lis->dato = ll1->dato;
+			lis->sig = NULL;
+
+			ll1 = ll1->sig;
+		}
+		else {
+			lis->dato = ll2->dato;
+			lis->sig = NULL;
+
+			ll2 = ll2->sig;
+		}
+
+
+		while (ll1 != NULL && ll2 != NULL) {
+
+			NodoLista* n = new NodoLista;
+			last->sig = n;
+
+			if (ll1->dato <= ll2->dato) {
+				n->dato = ll1->dato;
+				n->sig = NULL;
+
+				ll1 = ll1->sig;
+			}
+			else {
+				n->dato = ll2->dato;
+				n->sig = NULL;
+
+				ll2 = ll2->sig;
+			}
+
+			last = last->sig;
+
+		}
+		NodoLista* ult = ll1 == NULL ? ll2 : ll1;
+
+		while (ult != NULL) {
+			NodoLista* n = new NodoLista;
+			n->dato = ult->dato;
+			n->sig = NULL;
+
+			last->sig = n;
+			ult = ult->sig;
+
+			last = last->sig;
+		}
+
+		return lis;
+	}
 }
 
 NodoLista* intercalarRec(NodoLista * l1, NodoLista * l2)
 {
-	// IMPLEMENTAR SOLUCION
-	return NULL;
+	NodoLista* n = new NodoLista;
+
+	if (l1 == NULL && l2 == NULL) {
+		n = NULL;
+	}
+	else if (l1 == NULL) {
+		n->dato = l2->dato;
+		n->sig = intercalarRec(l1, l2->sig);
+	}
+	else if (l2 == NULL) {
+		n->dato = l1->dato;
+		n->sig = intercalarRec(l1->sig, l2);
+	}
+	else if (l1->dato <= l2->dato) {
+		n->dato = l1->dato;
+		n->sig = intercalarRec(l1->sig, l2);
+	}
+	else {
+		n->dato = l2->dato;
+		n->sig = intercalarRec(l1, l2->sig);
+	}
+
+	return n;
 }
 
 NodoLista* insComFin(NodoLista * l, int x)
 {
-	// IMPLEMENTAR SOLUCION
-	return NULL;
+	if (l != NULL) {
+		NodoLista* l1 = new NodoLista; //LISTA A DEVOLVER
+		NodoLista* lastL = l; //ARREGLAR LA FUNCION QUE BORRA AL FINAL
+		NodoLista* last = l1; // ULTIMO DE LA LISTA A DEVOLVER
+
+		l1->dato = x;
+		l1->sig = NULL;
+
+
+		while (lastL->sig != NULL) {
+			lastL = lastL->sig;
+		}
+
+		last->sig = new NodoLista;
+		last = last->sig;
+		last->dato = lastL->dato;
+		last->sig = invertirParcial(l);
+
+		while (last->sig != NULL) {
+			last = last->sig;
+		}
+
+		last->sig = new NodoLista;
+		last = last->sig;
+		last->dato = x;
+		last->sig = NULL;
+
+		return l1;
+	}
+	else {
+		NodoLista* n = new NodoLista;
+		n->dato = x;
+		n->sig = new NodoLista;
+		n->sig->dato = x;
+		n->sig->sig = NULL;
+
+		return n;
+	}
 }
 
 NodoLista* exor(NodoLista * l1, NodoLista * l2)
@@ -159,7 +275,19 @@ NodoLista* exor(NodoLista * l1, NodoLista * l2)
 
 void eliminarDuplicadosListaOrdenadaDos(NodoLista * &l)
 {
-	// IMPLEMENTAR SOLUCION
+	if (l != NULL) {
+		if (l->sig != NULL) {
+			if (l->sig->sig != NULL) {
+				if (l->dato == l->sig->dato) {
+					l->sig = l->sig->sig;
+					eliminarDuplicadosListaOrdenadaDos(l->sig);
+				}
+				else {
+					eliminarDuplicadosListaOrdenadaDos(l->sig);
+				}
+			}
+		}
+	}
 }
 
 bool palindromo(NodoLista * l)
